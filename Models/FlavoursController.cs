2b661250cd62968a7da2ee72950529d13ae455c1
46706 <46706@DESKTOP-A5MLQAA>
2020-03-12 16:05:32 +0100

    Changes to make photo file upload possible

--- Models/FlavoursController.cs
+++ Models/FlavoursController.cs
@@ -56,7 +56,7 @@ namespace icecreamshop.Models
 
         // POST: Flavours/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+  
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(FlavourCreateViewModel model)
